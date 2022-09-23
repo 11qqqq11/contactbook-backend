d69051c03cdfa71d48112680a57c5c1030638896
11qqqq11 <95013554+11qqqq11@users.noreply.github.com>
2022-09-23 09:59:50 +0700

    err

--- src/app.js
+++ src/app.js
@@ -9,25 +9,10 @@ app.use(cors());
 app.use(express.json());
 
 // Handle 404 response. 
-app.use((req, res, next) => {
-    // Handler for unknown route. 
-    // call next() to pass to the error handling middleware. 
-    return next(new ApiError(404, 'Resource not found'));
-});
 
 
-// Define error-handling middleware last, after other app.use() and routes calls. 
-app.use((error, req, res, next) => {
-    // The centralized error handling middleware 
-    // In any route handler, calling next(error) 
-    // will pass to this error handling middleware. 
-    return res.status(error.statusCode || 500).json({
-        message: error.message || 'Internal Server Error',
-    });
-});
 
-
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
     res.json({ message: 'welcome to contect book application.' });
 });
 
@@ -44,4 +29,21 @@ app.route('/api/contacts/:id')
     .put(contactController.update)
     .delete(contactController.delete);
 
+
+app.use((req, res, next) => {
+    // Handler for unknown route. 
+    // call next() to pass to the error handling middleware. 
+    return next(new ApiError(404, 'Resource not found'));
+});
+
+
+// Define error-handling middleware last, after other app.use() and routes calls. 
+app.use((error, req, res, next) => {
+    // The centralized error handling middleware 
+    // In any route handler, calling next(error) 
+    // will pass to this error handling middleware. 
+    return res.status(error.statusCode || 500).json({
+        message: error.message || 'Internal Server Error',
+    });
+});
 module.exports = app;
--- src/controllers/contact.controller.js
+++ src/controllers/contact.controller.js
@@ -1,21 +1,61 @@
-exports.create = (rea, res) => {
-    return res.send({ message: 'create handler' });
+const ContactService = require('../services/contact.service');
+const ApiError = require('../api-error');
+
+// Create and save a new Contact 
+exports.create = async (req, res, next) => {
+    console.log(req.body)
+    if (!req.body?.name) {
+        return next(new ApiError(400, 'Name can not be empty'));
+    }
+    try {
+        const contactService = new ContactService();
+        const contact = await contactService.create(req.body);
+        return res.send(contact);
+    } catch (error) {
+        console.log(error);
+        return next(
+            new ApiError(500, 'An error occurred while creating the contact')
+        );
+    }
 };
+
+
 exports.findAll = (req, res) => {
     return res.send({ message: 'findAll handler' });
 };
-exports.findOne = (reg, res) => {
+exports.findOne = (req, res) => {
     return res.send({ message: "findOne handler" });
 };
-exports.update = (reg, res) => {
+exports.update = (req, res) => {
     return res.send({ message: 'update handler' });
 };
-exports.delete = (reg, res) => {
+exports.delete = (req, res) => {
     return res.send({ message: 'delete handler' });
 };
-exports.deleteAll = (red, res) => {
+exports.deleteAll = (req, res) => {
     return res.send({ message: 'deleteAll handler' });
 };
-exports.findAllFavorite = (reg, res) => {
+exports.findAllFavorite = (req, res) => {
     return res.send({ message: 'findAllFavorite handler' });
+};
+
+// Retrieve all contacts of a user from the database 
+exports.findAll = async (req, res, next) => {
+    let contacts = [];
+    try {
+        const contactService = new ContactService();
+        const { name } = req.query;
+        if (name) {
+            contacts = await contactService.findByName(name);
+        } else {
+            contacts = await contactService.all();
+        }
+    } catch (error) {
+        console.log(error);
+        return next(
+            new ApiError(500, 'An error occurred while retrieving contacts')
+        );
+    }
+
+    return res.send(contacts);
 };
